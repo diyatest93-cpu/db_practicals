6e73ed5e8938c828a10f0ceedec8fa3e02b4c18d
Diya <diyatest93@gmail.com>
2026-08-18 09:57:05 +0530

    Prepare project for deployment

--- core/settings.py
+++ core/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-l_el9fgkcvhe)%8ua9q$zzc8lc&x!ax=0887wyof(w1+)s1aen'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -118,5 +118,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 MEDIA_URL='/media/'
 MEDIA_ROOT= BASE_DIR / 'media'
